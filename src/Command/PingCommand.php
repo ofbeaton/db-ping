82234f4bbe2a2c2091f00ce4415921fce6b0827f
Finlay Beaton <ofbeaton@gmail.com>
2016-08-05 01:46:07 -0400

    set modes again

--- src/Command/PingCommand.php
+++ src/Command/PingCommand.php
@@ -320,6 +320,10 @@ abstract class PingCommand extends Command
             return false;
         }
 
+        // make sure our modes are set
+        $this->dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->dbh->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+
         $this->writeReply('connected', $input, $output);
         return true;
     }//end connect()
