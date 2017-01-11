610d510629b14cce7358082aad754c6d8dabdf5a
Shadi Akiki <shadiakiki1986@gmail.com>
2017-01-11 15:14:08 +0200

    aesthetics for phpcs

--- src/Command/PingCommand.php
+++ src/Command/PingCommand.php
@@ -192,10 +192,12 @@ abstract class PingCommand extends Command
             'format',
             'f',
             InputOption::VALUE_OPTIONAL,
-            'Output string format: default is "tfmdresxa" to show timestamp,from,msg,delay,repeat,exec,success,fail. a stands for annotated, i.e. "delay=10" instead of "10". Drop letters to drop parts of the output',
+            'Output string format: default is "tfmdresxa" to show '
+            .'timestamp,from,msg,delay,repeat,exec,success,fail.'
+            .' a stands for annotated, i.e. "delay=10" instead of "10".'
+            .' Drop letters to drop parts of the output',
             'tfmdresxa'
         );
-
     }//end configure()
 
 
@@ -515,23 +517,59 @@ abstract class PingCommand extends Command
             $sinceBad = 0.0;
         }
         $msg = [
-            't'=>['time=',date('Y-m-d H:i:s'),''],
-            'f'=>['from ',$this->nickname($input),''],
-            'm'=>['',$msg,''],
-            'd'=>['delay=',$input->getOption('delay'),'ms'],
-            'r'=>['repeat=',$input->getOption('repeat'),'x'],
-            'e'=>['exec=',$this->execTime(),'ms'],
-            's'=>['since success=',$sinceGood,'s'],
-            'x'=>['since fail=',$sinceBad,'s']
-        ];
-        $format=str_split($input->getOption('format'));
-        if(!in_array('a',$format)) {
-          array_walk($msg,function(&$row) { $row = [$row[1]]; });
+                't' => [
+                        'time=',
+                        date('Y-m-d H:i:s'),
+                        '',
+                       ],
+                'f' => [
+                        'from ',
+                        $this->nickname($input),
+                        '',
+                       ],
+                'm' => [
+                        '',
+                        $msg,
+                        '',
+                       ],
+                'd' => [
+                        'delay=',
+                        $input->getOption('delay'),
+                        'ms',
+                       ],
+                'r' => [
+                        'repeat=',
+                        $input->getOption('repeat'),
+                        'x',
+                       ],
+                'e' => [
+                        'exec=',
+                        $this->execTime(),
+                        'ms',
+                       ],
+                's' => [
+                        'since success=',
+                        $sinceGood,
+                        's',
+                       ],
+                'x' => [
+                        'since fail=',
+                        $sinceBad,
+                        's',
+                       ],
+               ];
+        $format = str_split($input->getOption('format'));
+        if (in_array('a', $format) === false) {
+            array_walk($msg, function (&$row) {
+                $row = [$row[1]];
+            });
         }
-        array_walk($msg,function(&$row) { $row = implode('',$row); });
-        $msg = array_intersect_key($msg,array_flip($format));
-        $msg = array_replace(array_flip($format),$msg);
-        $msg = implode(', ',$msg);
+        array_walk($msg, function (&$row) {
+            $row = implode('', $row);
+        });
+        $msg = array_intersect_key($msg, array_flip($format));
+        $msg = array_replace(array_flip($format), $msg);
+        $msg = implode(', ', $msg);
         $output->writeln($msg);
     }//end writeReply()
 }//end class
